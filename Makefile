7c1305406cee3c579ca77927b13061928a2bda55
Tomilin Ilya <tomilin.inbox@gmail.com>
2020-05-05 15:22:27 +0300

    done

--- Makefile
+++ Makefile
@@ -10,7 +10,8 @@ clean:
 	rm -Rf ./bin/*
 
 build:
-	$(FC) $(FFLAGS) -c "./environment_x64.f90" -J "./obj/" -o "./obj/environment.o"
+	$(FC) $(FFLAGS) -c "./environment_x64.f90" -J "./obj/" -o \
+                       "./obj/environment.o"
 
 	$(FC) $(FFLAGS) -c "./modules/DECOMP.f90" -I "./obj/" -o "./obj/DECOMP.o"
 	$(FC) $(FFLAGS) -c "./modules/FEHL.f90"   -I "./obj/" -o "./obj/FEHL.o"
@@ -21,8 +22,6 @@ build:
 	$(FC) $(FFLAGS) -c "./modules/ZEROIN.f90" -I "./obj/" -o "./obj/ZEROIN.o"
 
 	$(FC) $(FFLAGS) -c "./src/main.f90" -I "./src/" -o "./obj/main.o"
-#	$(FC) $(FFLAGS) $(FOPT) -c "$(DIR)/src/funA.f90" -I "$(DIR)/obj/" -o "$(DIR)/obj/funA.o"
-#	$(FC) $(FFLAGS) $(FOPT) -o "$(DIR)/bin/app" "$(DIR)/obj/environment.o" "$(DIR)/obj/RKF45.o" "$(DIR)/obj/RKFS.o" "$(DIR)/obj/FEHL.o" "$(DIR)/obj/QUANC8.o"  "$(DIR)/obj/ZEROIN.o" "$(DIR)/obj/funA.o"	"$(DIR)/obj/SOLVE.o" "$(DIR)/obj/DECOMP.o" 
 	$(FC) $(FFLAGS) $(FOPT) -o "./bin/app" "./obj/DECOMP.o" "./obj/FEHL.o" \
                                "./obj/QUANC8.o" "./obj/RKF45.o" \
                                "./obj/RKFS.o" "./obj/SOLVE.o" "./obj/ZEROIN.o" \
--- src/main.f90
+++ src/main.f90
@@ -1,23 +1,48 @@
 real function function_e( t )
+
     real :: t
 
     function_e = 1.0 / sqrt( ( t ** 2 + 1 ) * ( 3 * t ** 2 + 4 ) )
-
-    ! nik
-    !function_e = ((1.0 - exp(-0.8*t))/(t*(1 + 1.6 * t)))
     return
+
+end
+
+
+real function function_a( x )
+
+    real :: x
+
+    function_a = x ** 2 - cos( x )
+    return
+
+end
+
+
+subroutine duffing( t,  &
+                    w,  &
+                    yp )
+
+    real :: t, w( 2 ), yp( 2 ), e
+
+    e = 0.200000
+
+    yp( 1 ) = w( 2 ) - ( e * w( 2 ) ** 3 )
+    yp( 2 ) = w( 1 )
+
+    return
+
 end
 
 
 program coursework
 
-    external function_e
+    external function_e, &
+             function_a, &
+             duffing
 
-    integer :: NOFUN = 0, &
-               i     = 0, &
-               j     = 0
+    integer :: NOFUN = 0
 
-    ! --------------------------- FUNCTION_E -------------------------
+    real :: b = 0.0
 
     ! QUANC8 vars
     real :: result_e     = 0.0, &
@@ -31,9 +56,29 @@ program coursework
 
     real, parameter :: const_value_e = 0.497286
 
+    ! ZEROIN vars
+    real :: result_a   = 0.0, &
+            ax         = 0.0, &
+            bx         = 0.0, &
+            tol        = 0.0, &
+            zeroin
+
+    real, parameter :: const_value_a = 1.213399
+
+    ! RKF45 vars
+    real :: rwork( 15 ), &
+            w( 2 ),      &
+            t,           &
+            tout,        &
+            tfinal,      &
+            tprint
+
+    integer :: iwork( 5 ), &
+               iflag,      &
+               neqn
+
     ! ----------------------------------------------------------------
 
-    print *, "Hello World!"
 
     ! -------------------------- CALC_QUANC8 -------------------------
 
@@ -52,13 +97,81 @@ program coursework
                  NOFUN,        &
                  flag )
 
-    print *, quanc_output
-
     result_e = const_value_e * quanc_output
-    ! nik
-    !result_e = ( quanc_output - 0.40874702 ) ** 4
-    print *, result_e
+    write ( *, '( A, F8.6 )' ) "E = ", result_e
 
     ! ----------------------------------------------------------------
+
+    ! -------------------------- CALC_ZEROIN -------------------------
+
+    ax  = -0.8
+    bx  = 0.8
+    tol = 1.e-06
+
+    result_a = zeroin( ax, bx, function_a, tol )
+    result_a = result_a * const_value_a
+    write ( *, '( A, F8.6 )' ) "A = ", result_a
+
+    ! ----------------------------------------------------------------
+
+    ! -------------------------- CALC_RKF45 --------------------------
+
+    neqn   = 2
+    w( 1 ) = result_a
+    w( 2 ) = b
+    t      = 0.000001
+    tfinal = 16
+    iflag  = 1
+    tout   = t
+    tprint = 0.4
+
+    10 call RKF45( duffing, &
+                   neqn,    &
+                   w,       &
+                   t,       &
+                   tout,    &
+                   relerr,  &
+                   abserr,  &
+                   iflag,   &
+                   rwork,   &
+                   iwork )
+
+    write ( *, 11 ) t, w( 1 ), w( 2 )
+    !write ( *, 11 ) t, w( 2 )
+    !write ( *, 11 ) t, w( 1 )
+    go to ( 80, 20, 30, 40, 50, 60, 70, 80 ), iflag
+
+    20 tout = tprint + t
+
+    if ( t.lt.tfinal ) go to 10
+        stop
+
+    30 write ( *, 31 ) relerr, abserr
+        go to 10
+    40 write ( *, 41 )
+        go to 10
+    50 abserr = 0.1e-07
+    write ( *, 31 ) relerr, abserr
+        go to 10
+    60 relerr = relerr * 10.0
+    write ( *, 31 ) relerr, abserr
+
+    iflag = 2
+    go to 10
+
+    70 print 71
+    iflag = 2
+    go to 10
+    80 write ( *, 81 )
+
+    ! ----------------------------------------------------------------
+    !11 format( '(', f10.2, 2x, '; ', f10.6, 2x, '; ', E14.6, ' )' )
+    11 format( f10.2, 2x, f10.6, 2x, E14.6 )
+    !11 format( ' (', f10.2, 2x, ';', E14.6, ')' )
+    31 format( ' ГPAHИЦЫ ПOГPEШHOCTEЙ ИЗMEHEHЫ  '/' RELERR=', E10.3, 2X, &
+               'ABSERR=', E10.3 )
+    41 format( ' MHOГO ШAГOB ' )
+    71 format( ' MHOГO BЫXOДOB ' )
+    81 format( ' HEПPABИЛЬHЫЙ BЫЗOB ' )
 
 end program coursework
